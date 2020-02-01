a80c2d3cc141bb8c7f21442fd9f639ec6c842989
Connor <cookeconnor98@gmail.com>
2020-02-01 16:10:36 -0600

    Added the full scene for engine game, and player/neutrino movement

---
+++ Chief O'Brien Simulator/Assets/EngineAvatarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineAvatarController : MonoBehaviour
+{
+    public Rigidbody2D player_physics;
+    public bool allow_infinite_jumping = true;
+    public float jump_force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            player_physics.AddForce(new Vector2(0, 1) * jump_force);
+        }
+    }
+}
---
+++ Chief O'Brien Simulator/Assets/NeutrinoMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NeutrinoMover : MonoBehaviour
+{
+    private float nextPosition = 0f;
+    private float initialY;
+    public float speed;
+
+    public GameObject Avatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if( 0.1 >= Math.Abs(this.transform.position.y - nextPosition))
+        {
+            nextPosition = initialY + (float)UnityEngine.Random.Range(-3, 3);
+        }
+        else
+        {
+            this.transform.position = Vector2.Lerp(this.transform.position, new Vector2(this.transform.position.x, nextPosition), speed*Time.deltaTime);
+        }
+
+    }
+}
