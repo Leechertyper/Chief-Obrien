2d5a5faa199e97f123a8450fa504f1fa9cf2ae30
deeox2 <johann.doell@gmail.com>
2020-02-01 16:38:48 -0600

    Created player-terminal and terminal-gamemaster interaction.

---
+++ Chief O'Brien Simulator/Assets/Scripts/TerminalInteraction.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalInteraction : MonoBehaviour
+{
+
+    [Header("Manager References")]
+    public GameMaster gameMaster;
+    public bool canCallManager = false;
+
+    [Header("Collision References")]
+    public Collider2D shadowCollider;
+    public GameObject playerObjectCollided;
+
+    [Header("Minigame Variables")]
+    public int minigameID;
+    public bool minigameReady;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (shadowCollider == null)
+        {
+            Debug.LogError("Shadow collidier not set in editor.");
+        }
+        if (gameMaster == null) {
+            Debug.LogError("Game master not set in editor.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canCallManager && Input.GetAxisRaw("Fire1") != 0) {
+            CallManager();
+        }
+    }
+
+    void CallManager() {
+        gameMaster.SetupMinigame(this.minigameID);
+        canCallManager = false;
+        minigameReady = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Player" && minigameReady)
+        {
+            Debug.Log(this.gameObject + " was touched by " + collision.gameObject);
+            playerObjectCollided = collision.gameObject;
+            canCallManager = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log(collision.gameObject + " stopped touching " + this.gameObject);
+            playerObjectCollided = null;
+            canCallManager = false;
+        }
+    }
+}
