f548677640cb43ff26a9282803a6fc8b94201a0f
flibby4 <nordicrest@gmail.com>
2020-02-01 19:01:13 -0600

    Committing a non-working push so Tom can work on something else.

---
+++ Chief O'Brien Simulator/Assets/Scripts/OnBoundaryContact.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class OnBoundaryContact : MonoBehaviour
+{
+    public BarSpawner ParentBarSpawner;
+    public bool isGoingLeft; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x >= 10)
+        {
+            Destroy(this.gameObject);
+            ParentBarSpawner.SpawnBar();
+        }        
+    }
+
+}
