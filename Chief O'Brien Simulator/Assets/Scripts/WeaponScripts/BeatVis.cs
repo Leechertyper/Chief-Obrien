9a343620bb97c1c91924abf008c9a380dc3fd0b8
Connor <cookeconnor98@gmail.com>
2020-02-02 04:48:59 -0600

    Completed the weapons minigame

---
+++ Chief O'Brien Simulator/Assets/Scripts/WeaponScripts/BeatVis.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatVis : MonoBehaviour
+{
+    //This will be determined by bpm
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    GameObject visualizer;
+
+    SpriteRenderer sprite;
+    float timer;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        timer = Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.x > visualizer.transform.position.x)
+        {
+            transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DestructiveCollider"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
---
+++ Chief O'Brien Simulator/Assets/Scripts/WeaponScripts/BeatVisualizerCorrectnessDisplay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatVisualizerCorrectnessDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCorrectnessLevel(int lvl)
+    {
+        GetComponent<Animator>().SetInteger("ComboLevel", lvl);
+    }
+
+    public void Input(bool correctness)
+    {
+        if (correctness)
+        {
+            GetComponent<Animator>().SetTrigger("Correct");
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Incorrect");
+        }
+    }
+
+    private IEnumerator WaitThenResetTriggers(bool correctness)
+    {
+        yield return new WaitForSeconds((float)0.099);
+        if (correctness)
+        {
+            GetComponent<Animator>().ResetTrigger("Correct");
+        }
+        else
+        {
+            GetComponent<Animator>().ResetTrigger("Incorrect");
+        }
+    }
+}
