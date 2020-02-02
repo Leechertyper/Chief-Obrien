8a28b4f75f93747c44a998d184c2726a8fefcdc7
deeox2 <johann.doell@gmail.com>
2020-02-02 13:24:39 -0600

    Added weapon minigame... with some bugs.

--- Chief O'Brien Simulator/Assets/Scripts/WeaponScripts/WeaponMinigameManager.cs
+++ Chief O'Brien Simulator/Assets/Scripts/WeaponScripts/WeaponMinigameManager.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class WeaponMinigameManager : MonoBehaviour
 {
+    public MinigameCameraManager camMan;
+    public GameObject minigameContainer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camMan.SetMainCameraToThisMinigame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void LoseMinigame()
-    {
+    public void LoseMinigame() {
         Debug.Log("Lost minigame.");
+        this.transform.parent.parent.GetComponent<MinigameManager>().LostMinigame();
+        ReturnControlToShip();
     }
 
-    public void WinMinigame()
-    {
+    public void WinMinigame() {
         Debug.Log("Won minigame.");
+        this.transform.parent.parent.GetComponent<MinigameManager>().WonMinigame();
+        ReturnControlToShip();
     }
+
+    private void ReturnControlToShip() {
+        camMan.ReturnMainCameraToShip();
+        Destroy(minigameContainer);
+    }
+
 }
